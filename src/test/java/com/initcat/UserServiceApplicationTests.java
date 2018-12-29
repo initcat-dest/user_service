0315f3215d8c0b3cebf7c5d2e0d6471417975458
libo <libo@xmiles.cn>
2018-12-29 16:42:18 +0800

    测试代码整理

--- src/test/java/com/initcat/UserServiceApplicationTests.java
+++ src/test/java/com/initcat/UserServiceApplicationTests.java
@@ -1,7 +1,10 @@
 package com.initcat;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserServiceApplicationTests {
 
+	@Autowired
+	StringEncryptor encryptor;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void jasyptTest() {
+
+		String url = encryptor.encrypt("jdbc:mysql://XXXX:3306/initcat?useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+		String name = encryptor.encrypt("XXXX");
+		String password = encryptor.encrypt("XXXX");
+		String ip = encryptor.encrypt("XXXX");
+		System.out.println(url+"----------------");
+		System.out.println(name+"----------------");
+		System.out.println(password+"----------------");
+		System.out.println(ip+"----------------");
+		Assert.assertTrue(name.length() > 0);
+		Assert.assertTrue(password.length() > 0);
 	}
 
 }
