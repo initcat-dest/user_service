0ff25742d7458188b1d19b04505f5378997f5e01
mongkuli <mongku@163.com>
2019-03-17 15:15:38 +0800

    注释掉dubbo

--- src/main/java/com/initcat/user/controller/TestController.java
+++ src/main/java/com/initcat/user/controller/TestController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
  * @date 2018/11/27
  */
 @RestController
+@RequestMapping("/helloTest")
 public class TestController {
 
     @RequestMapping("/hello")
--- src/main/java/com/initcat/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/initcat/user/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.initcat.user.service.impl;
 
 import com.alicp.jetcache.anno.Cached;
-import com.initcat.user.constant.redis.UserRedisConstant;
 import com.initcat.user.model.db.CommUser;
 import com.initcat.user.repository.CommUserRepository;
 import com.initcat.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.initcat.user.constant.redis.UserRedisConstant.USER_INFO;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,7 +16,7 @@ public class UserServiceImpl implements UserService {
     CommUserRepository commUserRepository;
 
     @Override
-    @Cached(name = UserRedisConstant.USER_INFO, key = "#userNumber", expire = 200)
+    @Cached(name = USER_INFO, key = "#userNumber", expire = 200)
     public CommUser login(String userNumber, String passWard, Integer userType) {
         return commUserRepository.findByNumber(userNumber);
     }
